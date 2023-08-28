f3bdf35b5afd374f532def93b7058a3523e29d03
Sumi <105615659+Pastel-Sumi@users.noreply.github.com>
2023-08-27 21:52:42 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,18 +2,94 @@ from ultralytics import YOLO
 import supervision as sv
 import numpy as np
 import cv2
+import threading
+
 import os
-from PIL import Image
 from datetime import datetime
 from supervision.draw.color import ColorPalette
-import streamlit as st
 
+import socket
+#import pickle
+#import struct
+#import streamlit as st
 
+current_db = 0
+db_time = {
+    85: 28800,
+    86: 22860,
+    87: 18144,
+    88: 14400,
+    89: 11412,
+    90: 9072,
+    91: 7200,
+    92: 5724,
+    93: 4536,
+    94: 3600,
+    95: 2844,
+    96: 2268,
+    97: 1800,
+    98: 1428,
+    99: 1134,
+    100: 900,
+    101: 714,
+    102: 564,
+    103: 450,
+    104: 354,
+    105: 282,
+    106: 225,
+    107: 178.2,
+    108: 141.6,
+    109: 112.8,
+    110: 89.4,
+    111: 70.8,
+    112: 56.4,
+    113: 44.64,
+    114: 35.43,
+    115: 29,
+}
+
+def listen_dB():
+    global current_db
+    # Restricciones para class_id = 1
+    db_offset_for_ear_off = 10
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(("localhost", 6000))
+        s.listen(1)
+        while True:
+            conn, addr = s.accept()
+            data = conn.recv(1024)
+            if data:
+                message = data.decode()
+                print(message)
+                current_db =message
+
+            conn.close()
+    except OSError as e:
+        print("Error al establecer la conexión:", str(e))
+
+def send_notification(message):
+    try:
+        # Establece la conexión con el módulo principal
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(("localhost", 5000))
+        s.sendall(message.encode())
+        s.close()
+    except ConnectionRefusedError:
+        print("No se pudo establecer conexión con el módulo principal")
 
 def main():
+    global current_db
     
-    frame_placeholder = st.empty()
-
+    #streamlit
+    #frame_placeholder = st.empty()
+    '''server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('127.0.0.1', 8080))
+    server_socket.listen(1)
+    print("Esperando conexión...")
+    connection, client_address = server_socket.accept()
+    print("cliente conectado:", client_address)
+    '''
     class_colors = {
         0: sv.Color(0, 0, 255),   # Azul
         1: sv.Color(255, 0, 0),   # Rojo
@@ -45,7 +121,7 @@ def main():
 
         labels = []
         for _, _, confidence, class_id, tracker_id in detections:
-            label = f"#{tracker_id} {model.model.names[class_id]} {confidence:0.2f}"
+            label = f"#{tracker_id} {model.model.names[class_id]} {confidence:0.2f} {current_db}"
             
             # Registra el tiempo de inicio de seguimiento si es la primera detección de este tracker_id
             if tracker_id not in tracker_tracking_start_times:
@@ -64,8 +140,9 @@ def main():
 
             
                     image_name = f"{source}_{duration.seconds}s_xdB_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{len(trackers_exceeded_limit)}.jpg"
-                    image_path = os.path.join(r"C:\Users\Esteban\Downloads\NoiseTrack", image_name)
+                    image_path = os.path.join(r"C:\Users\D4r4r\noisetrack_pmv\epptracker\tracker3", image_name)
                     cv2.imwrite(image_path, result.orig_img)
+
             
             labels.append(label)
             
@@ -75,12 +152,20 @@ def main():
             detections=detections,
             labels=labels
         )
-
         
+        #socket
+        '''frame = pickle.dumps(frame)
+        size = len(frame)
+        p = struct.pack('!I', size)
+        frame = p + frame
+        connection.sendall(frame)
+        '''
+        #stream en una ventana de cv2
+        cv2.imshow("yolov8", frame)
 
-        #cv2.imshow("yolov8", frame)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_placeholder.image(frame)
+        #streamlit
+        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        #frame_placeholder.image(frame)
 
         for tracker_id in tracker_tracking_start_times.copy():
             if tracker_id not in [tracker_id for _, _, _, _, tracker_id in detections]:
@@ -92,4 +177,9 @@ def main():
 
 
 if __name__ == "__main__":
+    notification_thread = threading.Thread(target=listen_dB)
+    notification_thread.daemon = True
+    notification_thread.start()   
     main()
+    
+    
