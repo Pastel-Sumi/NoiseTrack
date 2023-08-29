312160c2fa7f0b1dfee906929f66f1cc0babbeaa
Camila Arancibia <camilachillan99@gmail.com>
2023-08-29 13:56:53 -0400

    feat(Chart): add chart

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -1,95 +1,169 @@
 import ReactEcharts from "echarts-for-react"
-import React, { useEffect } from "react";
-import * as echarts from 'echarts';
-
-//var echarts = require('echarts');
-var element = document.getElementById('echarts-for-react ');
-// var myChart = echarts.init(chartDom, 'dark', {
-//     renderer: 'svg'
-//   });
-var options;
-function randomData() {
-    now = new Date(+now + oneDay);
-    value = value + Math.random() * 21 - 10;
-    return {
-      name: now.toString(),
-      value: [
-        [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
-        Math.round(value)
-      ]
-    };
-  }
-let data = [];
-let now = new Date(1997, 9, 3);
-let oneDay = 24 * 3600 * 1000;
-let value = Math.random() * 1000;
-for (var i = 0; i < 1000; i++) {
-  data.push(randomData());
-}
-options = {
-    title: {
-        text: 'Dynamic Data & Time Axis'
-      },
-      tooltip: {
-        trigger: 'axis',
-        formatter: function (params) {
-          params = params[0];
-          var date = new Date(params.name);
-          return (
-            date.getDate() +
-            '/' +
-            (date.getMonth() + 1) +
-            '/' +
-            date.getFullYear() +
-            ' : ' +
-            params.value[1]
-          );
-        },
-        axisPointer: {
-          animation: false
-        }
-      },
-      xAxis: {
-        type: 'time',
-        splitLine: {
-          show: false
-        }
-      },
-      yAxis: {
-        type: 'value',
-        boundaryGap: [0, '100%'],
-        splitLine: {
-          show: false
-        }
-      },
-      series: [
-        {
-          name: 'Fake Data',
-          type: 'line',
-          showSymbol: false,
-          data: data
-        }
-      ]
-    };
-  setInterval(function () {
-    for (var i = 0; i < 5; i++) {
-      data.shift();
-      data.push(randomData());
-    }
-    echarts.setOption({
-        series: [
-          {
-            data: data
-          }
-        ]
-      });
-  }, 1000);
+import React, { useState, useEffect } from 'react';
+import cloneDeep from 'lodash.clonedeep';
 
 export function Chart() {
+  const DEFAULT_OPTION = {
+    title: {
+      text:'Hello Echarts-for-react.',
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      data:["data1", "data2"]
+    },
+    toolbox: {
+      show: true,
+      feature: {
+        dataView: {readOnly: false},
+        restore: {},
+        saveAsImage: {}
+      }
+    },
+    grid: {
+      top: 60,
+      left: 30,
+      right: 60,
+      bottom:30
+    },
+    dataZoom: {
+      show: false,
+      start: 0,
+      end: 100
+    },
+    visualMap: {
+      show: false,
+      min: 0,
+      max: 1000,
+      color: ['#BE002F', '#F20C00', '#F00056', '#FF2D51', '#FF2121', '#FF4C00', '#FF7500',
+        '#FF8936', '#FFA400', '#F0C239', '#FFF143', '#FAFF72', '#C9DD22', '#AFDD22',
+        '#9ED900', '#00E500', '#0EB83A', '#0AA344', '#0C8918', '#057748', '#177CB0']
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: true,
+        data: (function (){
+          let now = new Date();
+          let res = [];
+          let len = 50;
+          while (len--) {
+            res.unshift(now.toLocaleTimeString().replace(/^\D*/,''));
+            now = new Date(now - 2000);
+          }
+          return res;
+        })()
+      },
+      {
+        type: 'category',
+        boundaryGap: true,
+        data: (function (){
+          let res = [];
+          let len = 50;
+          while (len--) {
+            res.push(50 - len + 1);
+          }
+          return res;
+        })()
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        scale: true,
+        name: "data1",
+        max: 20,
+        min: 0,
+        boundaryGap: [0.2, 0.2]
+      },
+      {
+        type: 'value',
+        scale: true,
+        name: "data2",
+        max: 1200,
+        min: 0,
+        boundaryGap: [0.2, 0.2]
+      }
+    ],
+    series: [
+      {
+        name:'data1',
+        type:'bar',
+        xAxisIndex: 1,
+        yAxisIndex: 1,
+        itemStyle: {
+          normal: {
+            barBorderRadius: 4,
+          }
+        },
+        animationEasing: 'elasticOut',
+        animationDelay: function (idx) {
+          return idx * 10;
+        },
+        animationDelayUpdate: function (idx) {
+          return idx * 10;
+        },
+        data:(function (){
+          let res = [];
+          let len = 50;
+          while (len--) {
+            res.push(Math.round(Math.random() * 1000));
+          }
+          return res;
+        })()
+      },
+      {
+        name:'data2',
+        type:'line',
+        data:(function (){
+          let res = [];
+          let len = 0;
+          while (len < 50) {
+            res.push((Math.random()*10 + 5).toFixed(1) - 0);
+            len++;
+          }
+          return res;
+        })()
+      }
+    ]
+  };
+
+  let count;
+  const [option, setOption] = useState(DEFAULT_OPTION);
+
+  function fetchNewData() {
+    const axisData = (new Date()).toLocaleTimeString().replace(/^\D*/,'');
+    const newOption = cloneDeep(option); // immutable
+    newOption.title.text = 'Hello Echarts-for-react.' + new Date().getSeconds();
+    const data0 = newOption.series[0].data;
+    const data1 = newOption.series[1].data;
+    data0.shift();
+    data0.push(Math.round(Math.random() * 1000));
+    data1.shift();
+    data1.push((Math.random() * 10 + 5).toFixed(1) - 0);
+
+    newOption.xAxis[0].data.shift();
+    newOption.xAxis[0].data.push(axisData);
+    newOption.xAxis[1].data.shift();
+    newOption.xAxis[1].data.push(count++);
+
+    setOption(newOption);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      fetchNewData();
+    }, 1000);
+
+    return () => clearInterval(timer);
+  });
   return (
-    <ReactEcharts
-      option={options}
-      style={{ width: "600px", height: "300px" }}
-    ></ReactEcharts>
+    
+      <ReactEcharts
+        option={option}
+        style={{ width: "600px", height: "300px" }}
+      ></ReactEcharts>
+    
   )
 }
