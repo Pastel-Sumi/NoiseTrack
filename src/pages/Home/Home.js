07356f3b8eebe0973242350e2560e606c202f737
rmcovarr <65862943+rmcovarr@users.noreply.github.com>
2023-09-25 21:22:17 -0300

    Agregado vista sonido + test calendario

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,9 +3,29 @@ import { Button } from "semantic-ui-react";
 
 import { Measurement } from "../../api";
 
+import { Chart } from "../../components/Monitoreo/monitoreo";
+
+import  { useState } from 'react';
+import Calendar from 'react-calendar';
+import './Calendar.css';
+
 const measurement = new Measurement();
 
 export function Home() {
+
+
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleToggleCalendar = () => {
+    setShowCalendar(!showCalendar);
+  };
+
+  const handleDateClick = (value) => {
+    setSelectedDate(value);
+    console.log('Día seleccionado:', value);
+    setShowCalendar(false); // Cierra el calendario después de seleccionar una fecha
+  };
 
   const handleMeasurement = async () => {
     try{
@@ -19,6 +39,22 @@ export function Home() {
     <div>
       <h1>Home Screen</h1>
       <Button primary onClick={handleMeasurement}>Decibeles</Button>
+      <div style={{ width: '100', display: 'flex', justifyContent: 'center' }}>
+  <Chart />
+</div>
+
+<div>
+      <h1>Calendario</h1>
+      <button onClick={handleToggleCalendar}>
+        {showCalendar ? 'Cerrar Calendario' : 'Abrir Calendario'}
+      </button>
+      {showCalendar && (
+        <Calendar
+          onChange={handleDateClick}
+          value={selectedDate}
+        />
+      )}
+    </div>
     </div>
   )
 }
