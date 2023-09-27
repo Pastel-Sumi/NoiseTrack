55aa26a9e3e2ca1b599576895fd79067c1cd046e
Esteban Manríquez <esteban.manriquez@sansano.usm.cl>
2023-09-27 16:28:11 -0300

    Plugin changes

--- movil/macos/Flutter/GeneratedPluginRegistrant.swift
+++ movil/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,18 +7,22 @@ import Foundation
 
 import cloud_firestore
 import firebase_analytics
+import firebase_auth
 import firebase_core
 import firebase_database
 import firebase_messaging
+import firebase_storage
 import flutter_local_notifications
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
