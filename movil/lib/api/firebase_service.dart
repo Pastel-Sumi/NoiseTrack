1c79a7a6d6ef9aa9ad7568f84dae27ab0eed9b47
Esteban Manríquez <esteban.manriquez@sansano.usm.cl>
2023-10-24 15:25:34 -0300

    Offline cache

--- movil/lib/api/firebase_service.dart
+++ movil/lib/api/firebase_service.dart
@@ -2,7 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 FirebaseFirestore db = FirebaseFirestore.instance;
 
+
 Future<List> getNotifications() async {
+  db.settings =  const Settings(persistenceEnabled: true, cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED);
   List notifications = [];
   CollectionReference collectionReferenceNotification = db.collection('alerts');
 
